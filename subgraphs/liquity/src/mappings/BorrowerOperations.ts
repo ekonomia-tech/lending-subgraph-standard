d665f222d5fd9cac69d434c8e15a4abbc952b789
Steve Pham <33653442+umphams@users.noreply.github.com>
2022-04-07 08:20:18 -0400

    Write and deploy closeTrove() scenario for subgraph

--- subgraphs/liquity/src/mappings/BorrowerOperations.ts
+++ subgraphs/liquity/src/mappings/BorrowerOperations.ts
@@ -1,6 +1,6 @@
 import { ethereum, Address, BigInt, BigDecimal, log } from '@graphprotocol/graph-ts'
 import { Event } from '../../generated/schema'
-import { Market } from '../../generated/schema'
+import { Market, AccountInMarket } from '../../generated/schema'
 import { getOrCreateLUSD, getOrCreateEther } from '../helpers/asset'
 import {
   TroveUpdated,
@@ -14,6 +14,8 @@ import {
   updateAccountStats,
 } from '../helpers/account'
 import { getMarket, updateMarketStats } from '../helpers/market'
+import { getOrCreateACM } from '../helpers/acm'
+
 import { createProtocol, getProtocol } from '../helpers/protocol'
 import {
   EthToLUSD,
@@ -122,57 +124,119 @@ export function handleTroveUpdated(event: TroveUpdated): void {
     return
   }
 
-  // get event entity id if not openTrove() scenario
-  let eventId = event.transaction.hash
-    .toHexString()
-    .concat('-')
-    .concat(event.transactionLogIndex.toString())
-  // generate new event entity
-  let eventEntry = new Event(eventId)
+  // 'closeTrove' triggers both 'REPAY' and 'WITHDRAW', 2 events within one.
+  // subgraph loads old protocol total for account. Added amounts of LUSD and Collateral are adjusted in other functions and eventHandlers.
+  // Then we subtract old total for account from all totals (protocol, market)
+  // Then we mark the account as repaid? Have to see how the schema is set up for it.
+  // Finally we close out the Trove by ^ and changing its collateral, and borrowing amounts to 0.
   if (operation == 'closeTrove') {
     //repay debt in full
-    //TroveUpdate has the updated values for the now closed trove I would assume in full.
-    // TODO: double check that it is in full, or if GAS COMPENSATION isn't factored here but factored elsewhere.
-    // Assuming it's in full, then I just need to update MarketStats() with the full delta, which in this case is just the total amounts
-    // updateAccountStats with just the full amounts too.
 
+    let eventId1 = event.transaction.hash
+      .toHexString()
+      .concat('-')
+      .concat(event.transactionLogIndex.toString())
+      .concat('-')
+      .concat('1')
+    // this is the collateral, no fees applied. Fees applied in LUSD.
+
+    let closeTroveEventEntry1 = new Event(eventId1)
     // TODO: I wonder if 'CLOSE' is a good addition to this field.
-    eventEntry.eventType = 'REPAY'
+    closeTroveEventEntry1.eventType = 'REPAY'
+
+    let acmOld = getOrCreateACM(account.id.concat('-').concat(market.id), account.id)
+    let accountOldDebt = acmOld.borrowed
+    // update event happens every time. Only difference is those highlighted below
+    closeTroveEventEntry1.market = market.id
+    closeTroveEventEntry1.account = account.id
+    closeTroveEventEntry1.blockTime = event.block.timestamp.toI32()
+    closeTroveEventEntry1.blockNumber = event.block.number.toI32()
+    // different and likely belongs in their own respective if statements.
+    closeTroveEventEntry1.amount = accountOldDebt
+    closeTroveEventEntry1.save()
+    updateMarketStats(market.id, closeTroveEventEntry1.eventType, accountOldDebt)
+    updateAccountStats(
+      protocol.id,
+      market.id,
+      account.id,
+      accountOldDebt,
+      closeTroveEventEntry1.eventType,
+    )
+
+    // second event within openTrove() scenario
+    let eventId2 = event.transaction.hash
+      .toHexString()
+      .concat('-')
+      .concat(event.transactionLogIndex.toString())
+      .concat('-')
+      .concat('2')
+    let closeTroveEventEntry2 = new Event(eventId2)
+    closeTroveEventEntry2.eventType = 'WITHDRAW'
+
+    let accountOldColl = acmOld.deposited
+
+    // update event happens every time. Only difference is those highlighted below
+    closeTroveEventEntry2.market = market.id
+    closeTroveEventEntry2.account = account.id
+    closeTroveEventEntry2.blockTime = event.block.timestamp.toI32()
+    closeTroveEventEntry2.blockNumber = event.block.number.toI32()
+    // different and likely belongs in their own respective if statements.
+    closeTroveEventEntry2.amount = accountOldColl
+    closeTroveEventEntry2.save()
+    updateMarketStats(market.id, closeTroveEventEntry2.eventType, accountOldColl)
+    updateAccountStats(
+      protocol.id,
+      market.id,
+      account.id,
+      accountOldColl,
+      closeTroveEventEntry2.eventType,
+    )
+    return
+
+    // emit TroveUpdated(msg.sender, 0, 0, 0, BorrowerOperation.closeTrove);
   }
-  // must be adjustTrove, and Trove must exist or else this event wouldn't be emitted I believe. TODO: confirm that.
-  // TODO: Also need to figure out how to discern which scenario it is within the AdjustTrove() event emission... we may need to bind to the contract to read the function _adjustTrove(). It will cause significant time usage in sync.
-  // TODO: Could go about it and:
-  // 1. Load the account, check to see if certain trove details have changed... if trove.debt != event.params.debt, then we know that more LUSD was withdrawn.
-  if ('Insert check if it is addColl(), withdrawColl(), WithdrawLUSD(), repayLUSD()') {
-  }
-  // update event happens every time. Only difference is those highlighted below
-  eventEntry.market = market.id
-  eventEntry.account = account.id
-  eventEntry.blockTime = event.block.timestamp.toI32()
-  eventEntry.blockNumber = event.block.number.toI32()
-  // different and likely belongs in their own respective if statements.
-  eventEntry.amount = borrowAmount
-  eventEntry.borrowRate = event.params.borrowRate
-  eventEntry.interestRateMode = event.params.borrowRateMode.toI32()
-  eventEntry.save()
-  updateMarketStats(market.id, eventEntry.eventType, borrowAmount)
-  updateAccountStats(protocol.id, market.id, account.id, borrowAmount, eventEntry.eventType)
-  /**
-   * @dev This is the addColl()) scenario
-   */
-  /**
-   * @dev This is the withdrawColl() scenario
-   */
-  /**
-   * @dev this is the withdrawLUSD() scenario
-   */
-  /**
-   * @dev this is the repayLUSD() scenario
-   */
-  /**
-   * @dev This is the closeTrove() scenario
-   * TroveUpdated emitted before the actual LUSD is moved from the msg.sender, decreased in the ActivePool contract, and burnt.
-   */
+
+  // // get event entity id if not openTrove() or closeTrove() scenario
+  // let eventId = event.transaction.hash
+  //   .toHexString()
+  //   .concat('-')
+  //   .concat(event.transactionLogIndex.toString())
+  // // generate new event entity
+  // let eventEntry = new Event(eventId)
+  // // must be adjustTrove, and Trove must exist or else this event wouldn't be emitted I believe. TODO: confirm that.
+  // // TODO: Also need to figure out how to discern which scenario it is within the AdjustTrove() event emission... we may need to bind to the contract to read the function _adjustTrove(). It will cause significant time usage in sync.
+  // // TODO: Could go about it and:
+  // // 1. Load the account, check to see if certain trove details have changed... if trove.debt != event.params.debt, then we know that more LUSD was withdrawn.
+  // if ('Insert check if it is addColl(), withdrawColl(), WithdrawLUSD(), repayLUSD()') {
+  // }
+  // // update event happens every time. Only difference is those highlighted below
+  // eventEntry.market = market.id
+  // eventEntry.account = account.id
+  // eventEntry.blockTime = event.block.timestamp.toI32()
+  // eventEntry.blockNumber = event.block.number.toI32()
+  // // different and likely belongs in their own respective if statements.
+  // eventEntry.amount = borrowAmount
+  // eventEntry.borrowRate = event.params.borrowRate
+  // eventEntry.interestRateMode = event.params.borrowRateMode.toI32()
+  // eventEntry.save()
+  // updateMarketStats(market.id, eventEntry.eventType, borrowAmount)
+  // updateAccountStats(protocol.id, market.id, account.id, borrowAmount, eventEntry.eventType)
+  // /**
+  //  * @dev This is the addColl()) scenario
+  //  */
+  // /**
+  //  * @dev This is the withdrawColl() scenario
+  //  */
+  // /**
+  //  * @dev this is the withdrawLUSD() scenario
+  //  */
+  // /**
+  //  * @dev this is the repayLUSD() scenario
+  //  */
+  // /**
+  //  * @dev This is the closeTrove() scenario
+  //  * TroveUpdated emitted before the actual LUSD is moved from the msg.sender, decreased in the ActivePool contract, and burnt.
+  //  */
 }
 
 export function handleLUSDBorrowingFeePaid(event: LUSDBorrowingFeePaid): void {}
